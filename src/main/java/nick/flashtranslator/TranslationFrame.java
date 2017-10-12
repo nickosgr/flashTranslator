d97699e436ef1ef2d409676fa7ebca250872d841
nickosgr <nickos.christou@gmail.com>
2017-10-12 16:12:41 +0300

    Translations credits in About fix

--- src/main/java/nick/flashtranslator/TranslationFrame.java
+++ src/main/java/nick/flashtranslator/TranslationFrame.java
@@ -28,7 +28,6 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.ListSelectionModel;
-import javax.swing.UIManager;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
@@ -577,8 +576,8 @@ public class TranslationFrame extends javax.swing.JFrame {
                 + "<html><h2>" + bundle.getString("LIBRARIES") + ":</h2>\n"
                 + " Google Gson Version 2.2.4\n"
                 + " Flagstone Software's Transform library v3.0.2"
-                + "\n\n"
-                + "<html><h2>Copyright 2014 NIKOLAOS CHRISTOU</h2>\n"
+                + "\n"
+                + "\n"
                 + "\n"
                 + "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
                 + "you may not use this file except in compliance with the License.\n"
@@ -590,7 +589,7 @@ public class TranslationFrame extends javax.swing.JFrame {
                 + "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
                 + "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
                 + "See the License for the specific language governing permissions and\n"
-                + "limitations under the License.</html>");
+                + "limitations under the License.");
     }//GEN-LAST:event_jMenuItem8ActionPerformed
 
     private void clearGui() {
